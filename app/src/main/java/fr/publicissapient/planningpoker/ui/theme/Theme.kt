d97ce1216687268887c38e54f63a7b9876b447b4
Benjamin Lacroix <blacroix@xebia.fr>
2020-11-08 18:45:17 +0100

    fab color changer with speed dialog

--- app/src/main/java/fr/publicissapient/planningpoker/ui/theme/Theme.kt
+++ app/src/main/java/fr/publicissapient/planningpoker/ui/theme/Theme.kt
@@ -1,11 +1,13 @@
 package fr.publicissapient.planningpoker.ui.theme
 
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.graphics.Color
 
-private val RedThemeColors = lightColors(
+val RedThemeColors = lightColors(
     primary = primaryRed,
     primaryVariant = primaryRed,
     onPrimary = Color.Black,
@@ -14,13 +16,51 @@ private val RedThemeColors = lightColors(
     onSecondary = Color.White
 )
 
+val BlueThemeColors = lightColors(
+    primary = primaryBlue,
+    primaryVariant = primaryBlue,
+    onPrimary = Color.Black,
+    secondary = primaryBlue,
+    secondaryVariant = primaryBlue,
+    onSecondary = Color.White
+)
+
+val YellowThemeColors = lightColors(
+    primary = primaryYellow,
+    primaryVariant = primaryYellow,
+    onPrimary = Color.Black,
+    secondary = primaryYellow,
+    secondaryVariant = primaryYellow,
+    onSecondary = Color.White
+)
+
+val GreenThemeColors = lightColors(
+    primary = primaryGreen,
+    primaryVariant = primaryGreen,
+    onPrimary = Color.Black,
+    secondary = primaryGreen,
+    secondaryVariant = primaryGreen,
+    onSecondary = Color.White
+)
+
 @Composable
 fun PlanningPokerTheme(
+    colors: Colors = RedThemeColors,
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = RedThemeColors,
+        colors = colors,
         typography = planningPokerTypography,
         content = content
     )
+}
+
+@Composable
+fun PlanningPokerMultipleColorsTheme(
+    currentColors: MutableState<Colors>,
+    content: @Composable () -> Unit
+) {
+    PlanningPokerTheme(colors = currentColors.value) {
+        content()
+    }
 }
