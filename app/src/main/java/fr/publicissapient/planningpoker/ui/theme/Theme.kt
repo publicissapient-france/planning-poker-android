815cb56c44c337eaee95f7c99831e6ca4bf68087
Benjamin Lacroix <blacroix@xebia.fr>
2020-11-10 12:03:24 +0100

    ⏪ remove overrides on colors

--- app/src/main/java/fr/publicissapient/planningpoker/ui/theme/Theme.kt
+++ app/src/main/java/fr/publicissapient/planningpoker/ui/theme/Theme.kt
@@ -5,42 +5,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.graphics.Color
 
 val RedThemeColors = lightColors(
     primary = primaryRed,
-    primaryVariant = primaryRed,
-    onPrimary = Color.Black,
-    secondary = primaryRed,
-    secondaryVariant = primaryRed,
-    onSecondary = Color.White
 )
 
 val BlueThemeColors = lightColors(
     primary = primaryBlue,
-    primaryVariant = primaryBlue,
-    onPrimary = Color.Black,
-    secondary = primaryBlue,
-    secondaryVariant = primaryBlue,
-    onSecondary = Color.White
 )
 
 val YellowThemeColors = lightColors(
     primary = primaryYellow,
-    primaryVariant = primaryYellow,
-    onPrimary = Color.Black,
-    secondary = primaryYellow,
-    secondaryVariant = primaryYellow,
-    onSecondary = Color.White
 )
 
 val GreenThemeColors = lightColors(
     primary = primaryGreen,
-    primaryVariant = primaryGreen,
-    onPrimary = Color.Black,
-    secondary = primaryGreen,
-    secondaryVariant = primaryGreen,
-    onSecondary = Color.White
 )
 
 @Composable
